2410a37180ea520d8274d06af40d2bf1ac050676
Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
2020-11-11 09:32:58 +0100

    Tests done for Card class Co-authored-by: <miniharald@gmail.com>, <nekcoj@gmail.com>, <janis_k@hotmail.com>

---
+++ src/com/company/Card.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Card {
+
+    private int power;
+    private String name;
+    private int currentPower;
+
+    public Card() {}
+
+    public Card(int power, String name) {
+        this.power = power;
+        this.name = name;
+        //this.currentPower = power;
+    }
+
+    public void decreasePower(int decreaseBy) {
+
+    }
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentPower() {
+        return currentPower;
+    }
+
+    public void setCurrentPower(int currentPower) {
+        this.currentPower = currentPower;
+    }
+}
---
+++ src/test/com/company/CardTest.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    private Card card;
+
+    @BeforeEach
+    void setUpBefore() {
+        card = new Card(10, "SuperGalaxyFaceMelter");
+        System.out.println("card: " + card);
+    }
+
+    @Test
+    void constructorTest() {
+        Assertions.assertNotNull(card, "Constructor returned null");
+    }
+
+    @Test
+    void decreasePower() {
+        card.decreasePower(3);
+        Assertions.assertEquals(7, card.getCurrentPower(), "Wrong current power!");
+    }
+
+    @Test
+    void getPower() {
+        Assertions.assertEquals(10, card.getPower(), "Wrong power");
+    }
+
+    @Test
+    void setPower() {
+        card.setPower(7);
+        Assertions.assertEquals(7, card.getPower(), "Wrong power");
+    }
+
+    @Test
+    void getName() {
+        Assertions.assertEquals("SuperGalaxyFaceMelter", card.getName(), "Wrong name!");
+    }
+
+    @Test
+    void setName() {
+        card.setName("Orange menace");
+        Assertions.assertEquals("Orange menace", card.getName(), "Wrong name!");
+    }
+
+    @Test
+    void getCurrentPower() {
+        Assertions.assertEquals(10, card.getCurrentPower(), "Wrong power!");
+    }
+
+    @Test
+    void setCurrentPower() {
+        card.setCurrentPower(card.getPower());
+        Assertions.assertEquals(10, card.getCurrentPower(), "Wrong power!");
+    }
+}
