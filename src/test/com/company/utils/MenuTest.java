eef53fff51e33fc0b724598d20da9455359b90bc
Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
2020-11-13 14:32:16 +0100

    Added test for Menu class Co-authored-by: Joacim <nekcoj@gmail.com> Co-authored-by: Janis <janis_k@hotmail.com> Co-authored-by: Alex <miniharald@gmail.com>

---
+++ src/test/com/company/utils/MenuTest.java
@@ -0,0 +1,38 @@
+package com.company.utils;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTest {
+
+    Menu menu;
+    ArrayList<MenuChoiceBaseClass> cardMenuList;
+
+    @BeforeEach
+    void setUp() {
+        cardMenuList = new ArrayList<>();
+        cardMenuList.add(new MenuChoiceFunction("Val 1", '1', this::handleCardMenu, 1));
+        cardMenuList.add(new MenuChoiceFunction("Val 2", '2', this::handleCardMenu, 2));
+        cardMenuList.add(new MenuChoiceFunction("Val 3", '3', this::handleCardMenu, 3));
+        menu = new Menu() {
+            @Override
+            public ArrayList<MenuChoiceBaseClass> setInitialMenu() {
+                return cardMenuList;
+            }
+        };
+    }
+
+    private Object handleCardMenu(Object o) {
+        return o;
+    }
+
+    @Test
+    void handleFunctionMenu() {
+        assertEquals(1, menu.handleFunctionMenu(true), "Wrong menu choice");
+    }
+}
