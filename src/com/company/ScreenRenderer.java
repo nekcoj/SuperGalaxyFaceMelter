fcfe6e85c2a338bff49cf1ea5d761b18715d0572
nekcoj <55090544+nekcoj@users.noreply.github.com>
2020-11-16 14:24:22 +0100

    added additional tests and functionality for screenrenderer, implemented TextUtil class for text colors.
    Co-Authored-By: Janis Karlsson <janis_k@hotmail.com>

--- src/com/company/ScreenRenderer.java
+++ src/com/company/ScreenRenderer.java
@@ -1,26 +1,77 @@
 package com.company;
 
 import com.company.interfaces.Renderer;
+import com.company.utils.TextUtil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ScreenRenderer implements Renderer {
 
-    private String topLeft = "╔";
-    private String lineHor = "═";
-    private String lineVert = "║";
-    private String topRight = "╗";
-    private String bottomLeft = "╚";
-    private String bottomRight = "╝";
-    private int cardWidth = 18;
-    private int cardHeight = 12;
+  private String topLeft = "╔";
+  private String lineHor = "═";
+  private String lineVert = "║";
+  private String topRight = "╗";
+  private String bottomLeft = "╚";
+  private String bottomRight = "╝";
+  public final static int CARD_WIDTH = 18;
+  private final static int CARD_HEIGHT = 12;
+  private static final int OFFSET = 3;
 
-    @Override
-    public void render(GameState gameState) {
+  @Override
+  public void render(GameState gameState) {
 
+  }
+
+  public String generatePrintString(ArrayList<Card> cards){
+    String [][] cardOutput = new String[cards.size()][CARD_HEIGHT];
+    for (int i = 0; i < cards.size(); i++){
+      var ref = new Object() {
+        int rowCounter = 0;
+      };
+      cardOutput[i][ref.rowCounter++] = generateRow(topLeft, lineHor, topRight);
+      cardOutput[i][ref.rowCounter++] = generateRow(lineVert, " ", lineVert);
+      cardOutput[i][ref.rowCounter++] = generateRow(lineVert, " ", lineVert);
+      cardOutput[i][ref.rowCounter++] = generateContentRow(generatePowerRow(cards.get(i)), 2);
+      cardOutput[i][ref.rowCounter++] = generateRow(lineVert, " ", lineVert);
+      String[] cardName = cards.get(i).getName().split(" ");
+      int finalI = i;
+      Arrays.stream(cardName).forEach(name -> cardOutput[finalI][ref.rowCounter++] = generateContentRow(generateNameRow(name), 1));
+      if(cardName.length < 4){
+        for (int j = cardName.length; j < 4; j++ ){
+          cardOutput[i][ref.rowCounter++] = generateRow(lineVert, " ", lineVert);
+        }
+      }
+      cardOutput[i][ref.rowCounter++] = generateRow(lineVert, " ", lineVert);
+      cardOutput[i][ref.rowCounter++] = generateRow(lineVert, " ", lineVert);
+      cardOutput[i][ref.rowCounter++] = generateRow(bottomLeft, lineHor, bottomRight);
     }
 
-    public String generatePrintString(ArrayList<Card> cards){
-        return "";
+    StringBuilder str = new StringBuilder();
+    for (int i = 0; i < CARD_HEIGHT; i++) {
+      for (int j = 0; j < cards.size(); j++) {
+        str.append(cardOutput[j][i]).append(" ".repeat(OFFSET));
+      }
+      str.append("\n");
     }
+    return str.toString();
+  }
+
+  public String generateContentRow(String input, int changeColorCount){
+    return String.format("%s%s%s", lineVert, TextUtil.centerText(input, CARD_WIDTH + changeColorCount * 9 - 2), lineVert);
+  }
+
+  public String generatePowerRow(Card card){
+    return String.format("[%s/%s]", TextUtil.pimpString(card.getCurrentPower(), TextUtil.LEVEL_BOLD), TextUtil.pimpString(card.getPower(), TextUtil.LEVEL_STRESSED));
+  }
+
+  public String generateRow(String firstLetter, String fillLetter, String lastLetter){
+    return String.format("%s%s%s", firstLetter, fillLetter.repeat(CARD_WIDTH - 2), lastLetter);
+  }
+
+  public String generateNameRow(String input){
+    return String.format("%s", TextUtil.pimpString(input, TextUtil.LEVEL_INFO));
+  }
 }
--- src/test/com/company/ScreenRendererTest.java
+++ src/test/com/company/ScreenRendererTest.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.utils.TextUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,5 +30,30 @@ class ScreenRendererTest {
   void generatePrintString() {
     System.out.println("-------- generatePrintString Test --------");
     assertEquals(0, sr.generatePrintString(cards).indexOf("╔"), "Missing top left corner!");
+  }
+
+  @Test
+  void generateRow(){
+    System.out.println("-------- generateRow Test --------");
+    String str = sr.generateRow("<", "!", ">");
+    assertEquals(str.length(), ScreenRenderer.CARD_WIDTH, "Got incorrect row length!");
+    assertTrue(str.charAt(0) == '<', "Wrong character at index 0!");
+    assertTrue(str.charAt(1) == '!', "Wrong character at index 1!");
+    assertTrue(str.charAt(str.length() - 1) == '>', "Wrong character at index .length() - 1!");
+  }
+
+  @Test
+  void generatePowerRow(){
+    System.out.println("-------- generatePowerRow --------");
+    String str = String.format("[%s/%s]", TextUtil.pimpString(10, TextUtil.LEVEL_BOLD), TextUtil.pimpString(10, TextUtil.LEVEL_STRESSED));
+    assertTrue(sr.generatePowerRow(cards.get(0)).contains(str), "Incorrect power row!");
+
+  }
+
+  @Test
+  void generateContentRow(){
+    System.out.println("-------- generateContentRow --------");
+    String contentTest = "Content Test";
+    assertTrue(sr.generateContentRow(contentTest, 0).contains(contentTest), "Got wrong value from generateContentRow!");
   }
 }
